530c85eb57b39191b5bfb7b3026bd5bb8a6b2fc3
epic-person-on <123142178+epic-person-on@users.noreply.github.com>
2023-04-26 11:12:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 let rice = 0;
 let ricePerClick = 1;
 let riceFarmCost = 10;
+let riceFarmCount = 0;
 let riceMillCost = 50;
+let riceMillCount = 0;
 let riceBowlCost = 100;
+let riceBowlCount = 0;
 
 const riceCounter = document.getElementById('rice-counter');
 const riceButton = document.getElementById('rice-button');
 const riceFarmButton = document.getElementById('rice-farm');
 const riceMillButton = document.getElementById('rice-mill');
 const riceBowlButton = document.getElementById('rice-bowl');
+const riceFarmCountElem = document.getElementById('rice-farm-count');
+const riceMillCountElem = document.getElementById('rice-mill-count');
+const riceBowlCountElem = document.getElementById('rice-bowl-count');
 
 riceButton.addEventListener('click', () => {
   rice += ricePerClick;
@@ -20,6 +26,7 @@ riceFarmButton.addEventListener('click', () => {
     rice -= riceFarmCost;
     ricePerClick += 1;
     riceFarmCost *= 2;
+    riceFarmCount++;
     riceFarmButton.textContent = `Rice Farm (Cost: ${riceFarmCost})`;
     updateRice();
   }
@@ -30,6 +37,7 @@ riceMillButton.addEventListener('click', () => {
     rice -= riceMillCost;
     ricePerClick += 5;
     riceMillCost *= 2;
+    riceMillCount++;
     riceMillButton.textContent = `Rice Mill (Cost: ${riceMillCost})`;
     updateRice();
   }
@@ -40,14 +48,30 @@ riceBowlButton.addEventListener('click', () => {
     rice -= riceBowlCost;
     ricePerClick += 10;
     riceBowlCost *= 2;
+    riceBowlCount++;
     riceBowlButton.textContent = `Rice Bowl (Cost: ${riceBowlCost})`;
     updateRice();
   }
 });
+
+function updateUpgradeCounts() {
+  riceFarmCountElem.textContent = riceFarmCount;
+  riceMillCountElem.textContent = riceMillCount;
+  riceBowlCountElem.textContent = riceBowlCount;
+}
 
 function updateRice() {
   riceCounter.textContent = `${rice} Rice`;
   riceFarmButton.textContent = `Rice Farm (Cost: ${riceFarmCost})`;
   riceMillButton.textContent = `Rice Mill (Cost: ${riceMillCost})`;
   riceBowlButton.textContent = `Rice Bowl (Cost: ${riceBowlCost})`;
+  updateUpgradeCounts();
 }
+
+setInterval(() => {
+  rice += riceFarmCount;
+  updateRice();
+}, 1000);
+
+updateRice();
+
